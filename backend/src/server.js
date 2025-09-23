5943cbc07e652328db749f84556f32d2713e2ad5
prince-kr-dev <princekumark2004@gmail.com>
2025-09-23 17:59:48 +0530

    CORS fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend
+  origin: "https://budget-mate-frqh.vercel.app", // your frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
